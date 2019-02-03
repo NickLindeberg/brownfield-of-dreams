a0185f3789787221beff77594b5c785d0636e6a1
stoic-plus <ricardoledesmadev@gmail.com>
2019-02-03 10:16:50 -0700

    updates spec for bookmarked_segments returning bookmark objects

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -23,45 +23,87 @@ RSpec.describe User, type: :model do
     end
   end
   describe 'instance methods' do
-    context '#bookmarks' do
+    context '#bookmarked_segments' do
       it 'returns a users bookmarks - all tutorial and video information (in order of tutorial title then video position)' do
         user = create(:user)
-        tutorial = create(:tutorial, title: "How to Tie Your Shoes")
-        video = create(:video, title: "The Bunny Ears Technique", tutorial: tutorial)
+        tutorial = Tutorial.create(title: "How to Tie Your Shoes",
+                                   description: Faker::HitchhikersGuideToTheGalaxy.marvin_quote,
+                                   thumbnail: "sdk",
+                                   playlist_id: Faker::Crypto.md5,
+                                   classroom: false,
+                                   created_at: Time.now,
+                                   updated_at: Time.now
+                                 )
+        video = Video.create(title: "The Bunny Ears Technique",
+                             description: Faker::SiliconValley.motto,
+                             video_id: Faker::Crypto.md5,
+                             thumbnail: "sdahdskahds",
+                             tutorial_id: tutorial.id,
+                             position: 0
+                           )
 
-        tutorial_2 = create(:tutorial, title: "How to Invade Russia")
-        video_2 = create(:video, title: "Get a Sweater", tutorial: tutorial_2, position: 2)
-        video_3 = create(:video, tutorial: tutorial_2, position: 1)
+       tutorial_2 = Tutorial.create(title: "How to Invade Russia",
+                                  description: Faker::HitchhikersGuideToTheGalaxy.marvin_quote,
+                                  thumbnail: "sddasdadadsa",
+                                  playlist_id: Faker::Crypto.md5,
+                                  classroom: false,
+                                  created_at: Time.now,
+                                  updated_at: Time.now
+                                )
+       video_2 = Video.create(title: "Get a Sweater",
+                            description: Faker::SiliconValley.motto,
+                            video_id: Faker::Crypto.md5,
+                            thumbnail: "slllllllhds",
+                            tutorial_id: tutorial_2.id,
+                            position: 2
+                          )
+       video_3 = Video.create(title: Faker::Pokemon.name,
+                           description: Faker::SiliconValley.motto,
+                           video_id: Faker::Crypto.md5,
+                           thumbnail: "kjdjjdjdj",
+                           tutorial_id: tutorial_2.id,
+                           position: 1
+                         )
+       video_4 = Video.create(title: Faker::Pokemon.name,
+                           description: Faker::SiliconValley.motto,
+                           video_id: Faker::Crypto.md5,
+                           thumbnail: "kjdjjdjdj",
+                           tutorial_id: tutorial_2.id,
+                           position: 3
+                         )
 
-        Bookmarks.create(user_id: user.id, tutorial_id: tutorial.id, video_id: video.id)
-        Bookmarks.create(user_id: user.id, tutorial_id: tutorial_2.id, video_id: video_2.id)
+        Bookmark.create(user_id: user.id, tutorial_id: tutorial.id, video_id: video.id)
+        Bookmark.create(user_id: user.id, tutorial_id: tutorial_2.id, video_id: video_2.id)
+        Bookmark.create(user_id: user.id, tutorial_id: tutorial_2.id, video_id: video_3.id)
 
-        user_bookmarks = user.bookmarks
+        user_bookmarks = user.bookmarked_segments
 
-        expect(user_bookmarks.first.title).to eq(tutorial_2.title)
-        expect(user_bookmarks.first.description).to eq(tutorial_2.description)
-        expect(user_bookmarks.first.thumbnail).to eq(tutorial_2.thumbnail)
-        expect(user_bookmarks.first.playlist_id).to eq(tutorial_2.playlist_id)
+        expect(user_bookmarks.first.tut_tile).to eq(tutorial_2.title)
+        expect(user_bookmarks.first.tut_desc).to eq(tutorial_2.description)
+        expect(user_bookmarks.first.tut_thumbnail).to eq(tutorial_2.thumbnail)
 
-        expect(user_bookmarks.first.videos.first.title).to eq(video_3.title)
-        expect(user_bookmarks.first.videos.first.description).to eq(video_3.description)
-        expect(user_bookmarks.first.videos.first.id).to eq(video_3.id)
-        expect(user_bookmarks.first.videos.first.tutorial_id).to eq(video_3.tutorial_id)
+        expect(user_bookmarks.first.title).to eq(video_3.title)
+        expect(user_bookmarks.first.description).to eq(video_3.description)
+        expect(user_bookmarks.first.id).to eq(video_3.id)
+        expect(user_bookmarks.first.tutorial_id).to eq(video_3.tutorial_id)
 
-        expect(user_bookmarks.first.videos.second.title).to eq(video_2.title)
-        expect(user_bookmarks.first.videos.second.description).to eq(video_2.description)
-        expect(user_bookmarks.first.videos.second.id).to eq(video_2.id)
-        expect(user_bookmarks.first.videos.second.tutorial_id).to eq(video_2.tutorial_id)
+        expect(user_bookmarks.second.tut_tile).to eq(tutorial_2.title)
+        expect(user_bookmarks.second.tut_desc).to eq(tutorial_2.description)
+        expect(user_bookmarks.second.tut_thumbnail).to eq(tutorial_2.thumbnail)
 
-        expect(user_bookmarks.second.title).to eq(tutorial.title)
-        expect(user_bookmarks.second.description).to eq(tutorial.description)
-        expect(user_bookmarks.second.thumbnail).to eq(tutorial.thumbnail)
-        expect(user_bookmarks.second.playlist_id).to eq(tutorial.playlist_id)
+        expect(user_bookmarks.second.title).to eq(video_2.title)
+        expect(user_bookmarks.second.description).to eq(video_2.description)
+        expect(user_bookmarks.second.id).to eq(video_2.id)
+        expect(user_bookmarks.second.tutorial_id).to eq(video_2.tutorial_id)
 
-        expect(user_bookmarks.second.videos.first.title).to eq(video.title)
-        expect(user_bookmarks.second.videos.first.description).to eq(video.description)
-        expect(user_bookmarks.second.videos.first.id).to eq(video.id)
-        expect(user_bookmarks.second.videos.first.tutorial_id).to eq(video.tutorial_id)
+        expect(user_bookmarks.third.tut_tile).to eq(tutorial.title)
+        expect(user_bookmarks.third.tut_desc).to eq(tutorial.description)
+        expect(user_bookmarks.third.tut_thumbnail).to eq(tutorial.thumbnail)
+
+        expect(user_bookmarks.third.title).to eq(video.title)
+        expect(user_bookmarks.third.description).to eq(video.description)
+        expect(user_bookmarks.third.id).to eq(video.id)
+        expect(user_bookmarks.third.tutorial_id).to eq(video.tutorial_id)
       end
     end
   end
