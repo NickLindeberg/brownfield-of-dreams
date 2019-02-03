cb1b6b675021cad9b09f8f7731954871a6fac9aa
stoic-plus <ricardoledesmadev@gmail.com>
2019-02-03 10:22:45 -0700

    adds explicit foreign_key to Bookmark

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,5 +1,5 @@
 class Bookmark < ApplicationRecord
-  belongs_to :user
-  belongs_to :tutorial
-  belongs_to :video  
+  belongs_to :user, foreign_key: "user_id"
+  belongs_to :tutorial, foreign_key: "tutorial_id"
+  belongs_to :video, foreign_key: "video_id"
 end
