879496a4a084608e3b38230112ea67a758ccbaba
stoic-plus <ricardoledesmadev@gmail.com>
2019-02-03 10:19:29 -0700

    Revert "adds #uniq_tutorials to User - distinct tutorial information (bookmarks then joining tutorials)"
    This reverts commit c1b3f694321ad8dd76c8b82cc85c1dab0f04bd2e.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,11 @@
 class User < ApplicationRecord
   has_many :bookmarks
   has_many :tutorials, through: :bookmarks
-  has_many :videos, through: :bookmarks
+  has_many :videos, through: :bookmarks  
 
   validates :email, uniqueness: true, presence: true
   validates :password, presence: true, if: :password
   validates_presence_of :first_name
   enum role: [:default, :admin]
   has_secure_password
-
-  def uniq_tutorials
-    bookmarks
-      .joins(:tutorial)
-      .select("DISTINCT tutorials.title AS tut_tile, tutorials.description AS tut_desc, tutorials.thumbnail AS tut_thumbnail")
-  end
-
-  def bookmarked_segments
-    uniq_tutorials.joins(:video).select("videos.*").order("tutorials.title, videos.position")
-  end
 end
