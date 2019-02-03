8aba72bd0c1411ed5e7b58d36244b87dab9763a3
Nick Lindeberg <lindebergnick@gmail.com>
2019-02-03 16:00:45 -0700

    Creates friendships model and controller.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_30_233928) do
+ActiveRecord::Schema.define(version: 2019_02_03_195917) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "friendships", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["friend_id"], name: "index_friendships_on_friend_id"
+    t.index ["user_id", "friend_id"], name: "index_friendships_on_user_id_and_friend_id", unique: true
+    t.index ["user_id"], name: "index_friendships_on_user_id"
+  end
 
   create_table "taggings", id: :serial, force: :cascade do |t|
     t.integer "tag_id"
@@ -69,6 +79,7 @@ ActiveRecord::Schema.define(version: 2019_01_30_233928) do
     t.datetime "updated_at", null: false
     t.string "github_key"
     t.boolean "activated", default: false
+    t.string "handle"
     t.index ["email"], name: "index_users_on_email"
   end
 
