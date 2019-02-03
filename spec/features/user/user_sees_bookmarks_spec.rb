0a52f8bd3571501d79fbbead95271db876ee9352
stoic-plus <ricardoledesmadev@gmail.com>
2019-02-03 14:36:39 -0700

    adds final spec for rendering bookmarks

--- spec/features/user/user_sees_bookmarks_spec.rb
+++ spec/features/user/user_sees_bookmarks_spec.rb
@@ -1,9 +1,3 @@
-# As a logged in user
-# When I visit '/dashboard'
-# Then I should see a list of all bookmarked segments under the Bookmarked Segments section
-# And they should be organized by which tutorial they are a part of
-# And the videos should be ordered by their position
-
 require 'rails_helper'
 
 describe 'Rendering Bookmarked Segments' do
@@ -16,40 +10,42 @@ describe 'Rendering Bookmarked Segments' do
       @video = create(:video, title: "The Bunny Ears Technique", tutorial: @tutorial)
 
       @tutorial_2 = create(:tutorial, title: "How to Invade Russia")
-      @video_2 = create(:video, title: "Get a Sweater", tutorial: @tutorial_2)
+      @video_2 = create(:video, title: "Get a Sweater", tutorial: @tutorial_2, position: 2)
+      @video_3 = create(:video, title: "Vodka", tutorial: @tutorial_2, position: 1)
+      @video_4 = create(:video, title: "Russia invades you actually", tutorial: @tutorial_2)
 
-      UserVideo.create(user_id: user.id, video_id: @video.id)
-      UserVideo.create(user_id: user.id, video_id: @video_2.id)
+      Bookmark.create(user_id: user.id, tutorial_id: @tutorial.id, video_id: @video.id)
+      Bookmark.create(user_id: user.id, tutorial_id: @tutorial_2.id, video_id: @video_2.id)
+      Bookmark.create(user_id: user.id, tutorial_id: @tutorial_2.id, video_id: @video_3.id)
 
       visit dashboard_path
     end
     it 'shows a list of all bookmarked segments under Bookmarked Segments' do
-      expect(page).to have_content("Bookmarked Segments")
-
       within '.bookmarks' do
-        expect(page).to have_css('.bookmark', count: 2)
+        expect(page).to have_content("Bookmarked Segments")
+        expect(page).to have_css('.bookmark', count: 5)
+        expect(page).to have_content("How to Tie Your Shoes")
         expect(page).to have_content("The Bunny Ears Technique")
+        expect(page).to have_content("How to Invade Russia")
         expect(page).to have_content("Get a Sweater")
+        expect(page).to have_content("Vodka")
       end
     end
-    xit 'shows a list of bookmarked segments organized by tutorial' do
-      within '.bookmarks' do
-        within "#tutorial-#{@tutorial.id}" do
-          expect(page).to have_css("#video-#{@video.id}")
-          expect(page).to have_content("The Bunny Ears Technique")
-        end
-        within "#tutorial-#{@tutorial_2.id}" do
-          expect(page).to have_css("#video-#{@video_2.id}")
-          expect(page).to have_content("Get a Sweater")
-        end
-      end
-    end
-    xit 'shows a list of all bookmarked segments ordered by position' do
-      expect(all('.tutorial')[0]).to have_content("How to Tie Your Shoes")
-      expect(all('.tutorial')[1]).to have_content("How to Invade Russia")
+    it 'shows a list of all bookmarked segments ordered by tutorial and position' do
+      expect(all('.bookmark')[0]).to have_css("#tutorial-#{@tutorial_2.id}")
+      expect(all('.bookmark')[1]).to have_css("#video-#{@video_3.id}")
+      expect(all('.bookmark')[2]).to have_css("#video-#{@video_2.id}")
+      expect(all('.bookmark')[3]).to have_css("#tutorial-#{@tutorial.id}")
+      expect(all('.bookmark')[4]).to have_css("#video-#{@video.id}")
 
-      expect(all('.video')[0]).to have_content("The Bunny Ears Technique")
-      expect(all('.video')[1]).to have_content("Get a Sweater")
+      expect(all('.tutorial')[0]).to have_content("#{@tutorial_2.title}")
+      expect(all('.tutorial')[1]).to have_content("#{@tutorial.title}")
+      expect(all('.video')[0]).to have_content("Segment: #{@video_3.title}")
+      expect(all('.video')[1]).to have_content("Segment: #{@video_2.title}")
+      expect(all('.video')[2]).to have_content("Segment: #{@video.title}")
+
+      expect(page).to_not have_content("#{@video_4.title}")
+      expect(page).to_not have_css("#video-#{@video_4.id}")
     end
   end
 end
