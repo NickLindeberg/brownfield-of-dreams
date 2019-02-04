48d0a51fd6eb712989824e278a804d7ead15c6e6
stoic-plus <ricardoledesmadev@gmail.com>
2019-02-04 13:07:48 -0700

    adds validation to video for position - integer and >= 0

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,4 +2,9 @@ class Video < ApplicationRecord
   has_many :bookmarks
   has_many :users, through: :bookmarks
   belongs_to :tutorial
+
+  validates :position, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 0
+  }
 end
